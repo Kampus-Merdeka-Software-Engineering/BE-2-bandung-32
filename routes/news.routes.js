d360fce13ae5448241ce7d5330f918ff32e50d72
Shanice07 <140237243+Shanice07@users.noreply.github.com>
2023-12-09 23:58:00 +0700

    Update Back-end

---
+++ routes/news.routes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const newsRoutes = express.Router();
+const { prisma } = require("../config/prisma");
+
+//get all news
+newsRoutes.get("/", async(req, res) => {
+    const news = await prisma.news.findMany({
+        include: {
+           Category: true,
+        },
+    });
+    res.status(200).send(news);
+});
+
+//create new news
+newsRoutes.post("/", async (req, res) => {
+    const newnews = await prisma.news.create({
+        data: {
+            id: req.body.id,
+            jenis: req.body.jenis,
+            judul: req.body.judul,
+            publish: req.body.publish,
+            sumber: req.body.sumber,
+            content: req.body.content,
+            gambar: req.body.gambar,
+            categoryId: parseInt(req.body.categoryId),
+        },
+    });
+    res.status(201).json({
+        message: "news created",
+        data: newnews,
+    });
+});
+
+//get news by id
+newsRoutes.get("/:categoryId", async (req, res) => {
+    const { categoryId } = req.params;
+    const news = await prisma.news.findMany({
+        where: {
+            categoryId: parseInt(categoryId),
+        },
+    });
+    res.status(200).send(news);
+});
+
+
+module.exports = { newsRoutes };
